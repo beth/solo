db0871b74728269f08e59fae9c77fd2310880fb9
BethJohnson <BethJohnson3@gmail.com>
2014-12-02 06:40:16 -0800

    Fixes bug-adding courses w/o titles, adds EOI requirement detection.

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,14 @@
 angular.module('grad', [])
 .controller('gradCtrl', function(){
   //TODO:
-  //1. Make drop downs for EOI
-  //2. Make functions that evaluate progress for graduation
-  //3. Make information things that show alternatives
-  //4. Make progress bars
-  //5. Make log in
-  //6. Make administrator view 
+  //[x] Make drop downs for EOI 
+  //[x] Make data structure for EOI exams 
+  //[ ] Make functions that evaluate progress for graduation
+  //[x] Make functions that evaluate test progress  
+  //[ ] Make information things that show alternatives
+  //[ ] Make progress bars
+  //[ ] Make log in
+  //[ ] Make administrator view 
 
   this.subjectOptions = [
     {name: "Select Subject Area", value: []},
@@ -15,7 +17,9 @@ angular.module('grad', [])
     {name: "English", value: ['English I', 'English II', 'English III', 'English IV']},
     {name: "History and Citizenship Skills", value: ['United States History', 'United States Government', 'Oklahoma History', 'Other']},
     {name: "Fine Arts or Speech", value: ['Music', 'Art', 'Drama', 'Speech']},
-    {name: "Electives", value: []},
+    {name: "Foreign Language",  value: ['Input Class Name']},
+    {name: "Computer Technology", value: ['Input Class Name']},
+    {name: "Electives", value: ['Input Class Name']},
   ];
 
   this.semesterOptions = ['Semester 1', 'Semester 2'];
@@ -29,6 +33,9 @@ angular.module('grad', [])
     {name: 'IP', value: 0, credits: 0}];
 
   this.subjectSelected = this.subjectOptions[0];
+  this.needInputBox = function(){
+    return (this.courseSelected === "Other" || this.courseSelected === "Input Class Name");
+  }
   // this.courseSelected = "";
   // this.semesterSelected = "";
   // this.gradeSelected = "";
@@ -45,15 +52,25 @@ angular.module('grad', [])
   this.eoiSelected = "";
   this.scoreSelected = "";
 
+
   this.courseList = {
     "Mathematics": [],
     "Laboratory Science":[],
     "English": [],
     "History and Citizenship Skills": [],
     "Fine Arts or Speech": [],
-    "Electives":[],
+    "Foreign Language": [],
+    "Computer Technology": [],
+    "Electives":[]
   };
 
+  this.testList = {};
+  for(var i =0; i<this.eoiTests.length;i++){
+    this.testList[this.eoiTests[i]] = {taken: 0,
+      passed: false};
+  }
+ 
+  this.creditRequirementsMet = false;
   this.percentMathRequirements = function(){
     var courses = {};
     var mathClasses = this.courseList["Mathematics"];
@@ -74,12 +91,33 @@ angular.module('grad', [])
     return credits/3;
   };
 
+  this.eoiRequirementsMet = false;
+  this.eoiRequirements = function(){
+    var countPassed = 0;
+    var requiredPassed = false;
+    if(this.testList['Algebra I'].passed && this.testList['English II'].passed){
+      requiredPassed = true;
+    }
+    for(var k in this.testList){
+      if(this.testList[k].passed){
+        countPassed++;
+      }
+    }
+
+    this.eoiRequirementsMet = (requiredPassed && countPassed >=5);
+    return (requiredPassed && countPassed >=5);
+  };
+
   this.addCourse = function(){
     //TODO: IF DUPLCIATE, ASK ABOUT OVERWRITING?!
     if(this.courseSelected === 'Other'){
+      console.log(this.subjectSelected.name);
       this.courseSelected = this.subjectSelected.name + ": " + this.courseName;
       this.subjectSelected.name = 'Electives';
+    }else if(this.courseSelected === 'Input Class Name'){
+        this.courseSelected = this.courseName;
     }
+    
 
     this.courseList[this.subjectSelected.name].push({
       course: this.courseSelected,
@@ -91,7 +129,16 @@ angular.module('grad', [])
     this.courseSelected = "";
     this.semesterSelected = "";
     this.gradeSelected = "";
-    this.courseTitle = "";
+    this.courseName = "";
+  };
+
+  this.addTest= function(){
+    //TODO: IF DUPLCIATE, ASK ABOUT OVERWRITING?!
+    this.testList[this.eoiSelected].taken++;
+    this.testList[this.eoiSelected].passed = this.scoreSelected.value > 0;
+    this.eoiSelected = "";
+    this.scoreSelected = "";
+    this.eoiRequirements(); 
   };
 });
 // 
