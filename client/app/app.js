fddca324560add8f072dbaaae73442fa44015d6a
BethJohnson <BethJohnson3@gmail.com>
2014-12-01 16:00:07 -0800

    Adds dropdown controls to select subject area, course, semester, grade.

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,40 @@
 angular.module('grad', [])
 .controller('gradCtrl', function(){
-  this.student = {};
-  this.grade = "defaultGrade";
-  this.school = "defaultSchool"
+
+  this.subjectOptions = [
+    {name: "none selected", value: []},
+    {name: "Mathematics", value: ['Algebra I', 'Algebra II', 'Geometry', 'Trigonometry', 'Math Analysis', 'Calculus', 'AP Statistics', 'Other']},
+    {name: "Laboratory Science", value: ['Biology', 'Chemistry', 'Physics', 'Other']},
+    {name: "English", value: ['English I', 'English II', 'English III', 'English IV']},
+    {name: "History and Citizenship Skills", value: ['United States History', 'United States Government', 'Oklahoma History', 'Other']},
+    {name: "Fine Arts or Speech", value: ['Music', 'Art', 'Drama', 'Speech']},
+    {name: "Electives", value: []},
+  ];
+
+  this.semesterOptions = ['Semester 1', 'Semester 2'];
+
+  this.gradeOptions = [
+    {name: 'A', value: 4, credits: 0.5}, 
+    {name: 'B', value: 3, credits: 0.5}, 
+    {name: 'C', value: 2, credits: 0.5},
+    {name: 'D', value: 1, credits: 0.5},
+    {name: 'F', value: 0, credits: 0.5},
+    {name: 'IP', value: 0, credits: 0}];
+
+  this.subjectSelected = this.subjectOptions[0];
+  this.courseSelected = "";
+  this.semesterSelected = "";
+  this.gradeSelected = "";
+  this.courseTitle = "";
+
+  this.courseList = {
+    "Mathematics": [],
+    "Laboratory Science":[],
+    "English": [],
+    "History and Citizenship Skills": [],
+    "Fine Arts or Speech": [],
+    "Electives":[],
+  };
 });
 // 
 
